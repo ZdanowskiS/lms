9ce503f91308e274d05e94b54830bb1b198173ab
Sylwester Zdanowski <sylwesterzdanowski@gmail.com>
2019-07-15 21:49:42 +0200

    plugins/cleanup: fields description and translations cleanup

--- plugins/UStatsPlugin/lib/locale/pl/strings.php
+++ plugins/UStatsPlugin/lib/locale/pl/strings.php
@@ -1,23 +1,25 @@
 <?php
 
+$_LANG['Activity In Time Report'] = 'Raport aktywności w czasie';
 $_LANG['Assignments'] = 'Zobowiązania';
-
 $_LANG['asc'] = 'rosnąco';
 $_LANG['Closed Events'] = 'Zamknięte zdarzenia';
 $_LANG['Closed Assigned Events'] = 'Zamknięte przypisane zdarzenia';
 $_LANG['desc'] = 'malejąco';
 $_LANG['Events'] = 'Zdarzenia';
 $_LANG['Elements:'] = 'Elementy:';
-
-$_LANG['Order By:'] = 'Sortuj po:';
 $_LANG['High:'] = 'Górne:';
+$_LANG['Highest to mark'] = 'Najwyższe do oznaczenia' ;
 $_LANG['Low:'] = 'Dolne:';
+$_LANG['Lowest to mark'] = 'Najniższe do oznaczenia' ;
 $_LANG['Mark:'] = 'Oznacz:';
+$_LANG['New Messages'] = 'Nowe wiadomości';
+$_LANG['Order By:'] = 'Sortuj po:';
+$_LANG['Position'] = 'Stanowisko';
+$_LANG['Select week day'] = 'Wybierz dzień tygodnia';
 $_LANG['User Activity Reports'] = 'Raporty aktywności użytkowników';
 $_LANG['User Activity Report'] = 'Raport aktywności użytkowników';
-$_LANG['Activity In Time Report'] = 'Raport aktywności w czasie';
-$_LANG['Position'] = 'Stanowisko';
-
+$_LANG['User Report'] = 'Raport użytkowników';
 $_LANG['Week day:'] = 'Dzień tygodnia:';
 
 $_LANG['Monday'] = 'Poniedziałek';
