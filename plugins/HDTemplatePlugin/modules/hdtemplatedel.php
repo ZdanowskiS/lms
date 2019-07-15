5457cf99ed5709d06992c6a1b9d10eeb5327ab8c
Sylwester Zdanowski <sylwesterzdanowski@gmail.com>
2019-07-15 12:22:33 +0200

    plugins/bugfix: Fixed template delete, missing semicolon

--- plugins/HDTemplatePlugin/modules/hdtemplatedel.php
+++ plugins/HDTemplatePlugin/modules/hdtemplatedel.php
@@ -3,7 +3,7 @@
 $id = intval($_GET['id']);
 
 if ($id && $_GET['is_sure'] == '1') {
-	$DB->Execute('DELETE FROM hdtemplates WHERE id = ?', array($id))
+	$DB->Execute('DELETE FROM hdtemplates WHERE id = ?', array($id));
 }
 
 header('Location: ?m=hdtemplatelist');
